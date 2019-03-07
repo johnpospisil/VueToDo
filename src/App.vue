9a84cf3d13b6d51e3065a7345722d79bfda40b14
johnpospisil <audiofreak7@gmail.com>
2019-03-07 12:16:21 -0800

    Show pre-defined todo list items on the screen

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
   <div id="app">
-    
+    <Todos v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
+import Todos from './components/Todos';
 
 export default {
   name: 'app',
   components: {
-
+    Todos
+  },
+  // a function named 'data'
+  data() {
+    return {
+      // an array of todo objects
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Todos</h1>
+        <!-- Loop through todos items -->
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <h3>{{todo.title}}</h3>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Todos",
+    props: ["todos"]
+}
+</script>
+ 
+ <style scoped>
+     
+ </style>
